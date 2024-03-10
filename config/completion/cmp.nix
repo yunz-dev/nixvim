412cf6d13ff9fdf31b5d0519378d833245af7d2a
spector700 <72362399+spector700@users.noreply.github.com>
2024-03-09 20:26:04 -0600

    Fix: cmp now works correctly again

--- config/completion/cmp.nix
+++ config/completion/cmp.nix
@@ -1,5 +1,10 @@
 {
   plugins = {
+    cmp-nvim-lsp.enable = true;
+    cmp-emoji.enable = true;
+    cmp-buffer.enable = true;
+    cmp-path.enable = true;
+    cmp_luasnip.enable = true;
     cmp = {
       enable = true;
       # Preselect first entry
@@ -21,131 +26,85 @@
         ];
 
         window = {
-          completion = {
-            border = "rounded";
-            # winhighlight = "Normal:Normal,FloatBorder:FloatBorder,CursorLine:Visual,Search:None";
-          };
-          documentation = {
-            border = "rounded";
-          };
+          completion.border = "rounded";
+          documentation.border = "rounded";
         };
         experimental.ghost_text = true;
-      };
 
-      cmdline = {
-        "<Down>" = {
-          modes = [ "i" "s" ];
-          action = ''
-             function(fallback)
-             	if cmp.visible() then
-            		cmp.select_next_item()
-            elseif luasnip.expand_or_jumpable() then
-            	luasnip.expand_or_jump()
-            else
-            fallback()
-                 end
+        mapping = {
+          "<C-n>" = "cmp.mapping.select_next_item()";
+          "<C-p>" = "cmp.mapping.select_prev_item()";
+          "<C-e>" = "cmp.mapping.close()";
+          "<C-d>" = "cmp.mapping.scroll_docs(-4)";
+          "<C-f>" = "cmp.mapping.scroll_docs(4)";
+          "<C-Space>" = "cmp.mapping.complete()";
+          "<CR>" = "cmp.mapping.confirm({ behavior = cmp.ConfirmBehavior.Insert, select = true })";
+          "<Up>" = "cmp.mapping(cmp.mapping.select_prev_item(), {'i', 's'})";
+          "<Down>" = "cmp.mapping(cmp.mapping.select_next_item(), {'i', 's'})";
+        };
+        snippet = {
+          expand = ''
+            function(args)
+              require('luasnip').lsp_expand(args.body)
             end
           '';
         };
-        "<Up>" = {
-          modes = [ "i" "s" ];
-          action = ''
-                 function(fallback)
-            	if cmp.visible() then
-            		cmp.select_prev_item()
-            	elseif luasnip.jumpable(-1) then
-            		luasnip.jump(-1)
-            	else
-            		fallback()
-            	end
-            end
-          '';
-        };
-        "<C-n>" = {
-          action = "cmp.mapping.select_next_item()";
-        };
-        "<C-p>" = {
-          action = "cmp.mapping.select_prev_item()";
-        };
-        "<C-e>" = {
-          action = "cmp.mapping.abort()";
-        };
-        "<C-b>" = {
-          action = "cmp.mapping.scroll_docs(-4)";
-        };
-        "<C-f>" = {
-          action = "cmp.mapping.scroll_docs(4)";
-        };
-        "<C-Space>" = {
-          action = "cmp.mapping.complete()";
-        };
-        "<CR>" = {
-          action = "cmp.mapping.confirm({ select = true })";
-        };
-        "<S-CR>" = {
-          action = "cmp.mapping.confirm({ behavior = cmp.ConfirmBehavior.Replace, select = true })";
+        formatting = {
+          fields = [ "abbr" "kind" "menu" ];
+          format =
+            # lua
+            ''
+              function(_, item)
+                local icons = {
+                  Namespace = "󰌗",
+                  Text = "󰊄",
+                  Method = "󰆧",
+                  Function = "󰡱",
+                  Constructor = "",
+                  Field = "󰜢",
+                  Variable = "󰀫",
+                  Class = "󰠱",
+                  Interface = "",
+                  Module = "󰕳",
+                  Property = "",
+                  Unit = "󰑭",
+                  Value = "󰎠",
+                  Enum = "",
+                  Keyword = "󰌋",
+                  Snippet = "",
+                  Color = "󰏘",
+                  File = "󰈚",
+                  Reference = "󰈇",
+                  Folder = "󰉋",
+                  EnumMember = "",
+                  Constant = "󰏿",
+                  Struct = "󰙅",
+                  Event = "",
+                  Operator = "󰆕",
+                  TypeParameter = "",
+                  Table = "",
+                  Object = "󰅩",
+                  Tag = "",
+                  Array = "[]",
+                  Boolean = "",
+                  Number = "",
+                  Null = "󰟢",
+                  String = "󰉿",
+                  Calendar = "",
+                  Watch = "󰥔",
+                  Package = "",
+                  Copilot = "",
+                  Codeium = "",
+                  TabNine = "",
+                }
+
+                local icon = icons[item.kind] or ""
+                item.kind = string.format("%s %s", icon, item.kind or "")
+                return item
+              end
+            '';
         };
       };
     };
-    cmp-nvim-lsp.enable = true;
-    cmp-buffer.enable = true;
-    cmp-path.enable = true;
-    cmp_luasnip.enable = true;
   };
-  extraConfigLua = ''
-        luasnip = require("luasnip")
-        kind_icons = {
-          Text = "󰊄",
-          Method = "",
-          Function = "󰡱",
-          Constructor = "",
-          Field = "",
-          Variable = "󱀍",
-          Class = "",
-          Interface = "",
-          Module = "󰕳",
-          Property = "",
-          Unit = "",
-          Value = "",
-          Enum = "",
-          Keyword = "",
-          Snippet = "",
-          Color = "",
-          File = "",
-          Reference = "",
-          Folder = "",
-          EnumMember = "",
-          Constant = "",
-          Struct = "",
-          Event = "",
-          Operator = "",
-          TypeParameter = "",
-        } 
-
-     local cmp = require'cmp'
-    -- Set configuration for specific filetype.
-     cmp.setup.filetype('gitcommit', {
-       sources = cmp.config.sources({
-         { name = 'cmp_git' }, -- You can specify the `cmp_git` source if you were installed it.
-       }, {
-         { name = 'buffer' },
-       })
-     })
-
-     -- Use buffer source for `/` (if you enabled `native_menu`, this won't work anymore).
-     cmp.setup.cmdline({ '/', "?" }, {
-       sources = {
-         { name = 'buffer' }
-       }
-     })
-
-     -- Use cmdline & path source for ':' (if you enabled `native_menu`, this won't work anymore).
-     cmp.setup.cmdline(':', {
-       sources = cmp.config.sources({
-         { name = 'path' }
-       }, {
-         { name = 'cmdline' }
-       })
-     })  
-  '';
 }
