b05ca58a2350777c2318dd934b2bcba9e8d9b7f2
spector700 <72362399+spector700@users.noreply.github.com>
2024-03-09 21:09:37 -0600

    feat: Added colors to bufferline

---
+++ config/ui/bufferline.nix
@@ -0,0 +1,168 @@
+{
+  plugins = {
+    bufferline = {
+      enable = true;
+      highlights = {
+        background = {
+          bg = "#252434";
+          fg = "#605f6f";
+        };
+
+        bufferSelected = {
+          bg = "#1E1D2D";
+          fg = "#D9E0EE";
+        };
+        bufferVisible = {
+          fg = "#605f6f";
+          bg = "#252434";
+        };
+
+        error = {
+          fg = "#605f6f";
+          bg = "#252434";
+        };
+        errorDiagnostic = {
+          fg = "#605f6f";
+          bg = "#252434";
+        };
+
+        closeButton = {
+          fg = "#605f6f";
+          bg = "#252434";
+        };
+        closeButtonVisible = {
+          fg = "#605f6f";
+          bg = "#252434";
+        };
+        fill = {
+          bg = "#1E1D2D";
+          fg = "#605f6f";
+        };
+        indicatorSelected = {
+          bg = "#1E1D2D";
+          fg = "#1E1D2D";
+        };
+
+        modified = {
+          fg = "#F38BA8";
+          bg = "#252434";
+        };
+        modifiedVisible = {
+          fg = "#F38BA8";
+          bg = "#252434";
+        };
+        modifiedSelected = {
+          fg = "#ABE9B3";
+          bg = "#1E1D2D";
+        };
+
+        separator = {
+          bg = "#252434";
+          fg = "#252434";
+        };
+        separatorVisible = {
+          bg = "#252434";
+          fg = "#252434";
+        };
+        separatorSelected = {
+          bg = "#252434";
+          fg = "#252434";
+        };
+
+        duplicate = {
+          fg = "NONE";
+          bg = "#252434";
+        };
+        duplicateSelected = {
+          fg = "#F38BA8";
+          bg = "#1E1D2D";
+        };
+        duplicateVisible = {
+          fg = "#89B4FA";
+          bg = "#252434";
+        };
+      };
+      offsets = [
+        {
+          filetype = "neo-tree";
+          text = "Neo-tree";
+          highlight = "Directory";
+          text_align = "left";
+        }
+      ];
+    };
+  };
+  keymaps = [
+    {
+      mode = "n";
+      key = "<C-Tab>";
+      action = "<cmd>BufferLineCycleNext<cr>";
+      options = {
+        desc = "Cycle to next buffer";
+      };
+    }
+
+    {
+      mode = "n";
+      key = "<C-S-Tab>";
+      action = "<cmd>BufferLineCyclePrev<cr>";
+      options = {
+        desc = "Cycle to previous buffer";
+      };
+    }
+
+    {
+      mode = "n";
+      key = "<C-x>";
+      action = "<cmd>bdelete<cr>";
+      options = {
+        desc = "Delete buffer";
+      };
+    }
+
+    {
+      mode = "n";
+      key = "<leader>br";
+      action = "<cmd>BufferLineCloseRight<cr>";
+      options = {
+        desc = "Delete buffers to the right";
+      };
+    }
+
+    {
+      mode = "n";
+      key = "<leader>bl";
+      action = "<cmd>BufferLineCloseLeft<cr>";
+      options = {
+        desc = "Delete buffers to the left";
+      };
+    }
+
+    {
+      mode = "n";
+      key = "<leader>bo";
+      action = "<cmd>BufferLineCloseOthers<cr>";
+      options = {
+        desc = "Delete other buffers";
+      };
+    }
+
+    {
+      mode = "n";
+      key = "<leader>bp";
+      action = "<cmd>BufferLineTogglePin<cr>";
+      options = {
+        desc = "Toggle pin";
+      };
+    }
+
+    {
+      mode = "n";
+      key = "<leader>bP";
+      action = "<Cmd>BufferLineGroupClose ungrouped<CR>";
+      options = {
+        desc = "Delete non-pinned buffers";
+      };
+    }
+  ];
+}
