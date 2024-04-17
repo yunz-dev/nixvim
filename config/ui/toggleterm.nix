4b51ec4fbfd1306e3fe9aa41d31713dddfdcc368
spector700 <72362399+spector700@users.noreply.github.com>
2024-04-17 15:56:51 -0500

    fix: setting renames

--- config/ui/toggleterm.nix
+++ config/ui/toggleterm.nix
@@ -1,29 +1,32 @@
 {
   plugins.toggleterm = {
     enable = true;
-    size = ''
-      function(term)
-        if term.direction == "horizontal" then
-          return 30
-      elseif term.direction == "vertical" then
-          return vim.o.columns * 0.4
+    settings = {
+      size = ''
+        function(term)
+          if term.direction == "horizontal" then
+            return 30
+        elseif term.direction == "vertical" then
+            return vim.o.columns * 0.4
+          end
         end
-      end
-    '';
-    openMapping = "<C-/>";
-    shadeTerminals = true;
-    startInInsert = true;
-    terminalMappings = true;
-    persistMode = true;
-    insertMappings = true;
-    closeOnExit = true;
-    shell = "zsh";
-    direction = "horizontal"; # 'vertical' | 'horizontal' | 'tab' | 'float'
-    floatOpts = {
-      border = "single"; # 'single' | 'double' | 'shadow' | 'curved' | ... other options supported by win open
-      width = 80;
-      height = 20;
-      winblend = 0;
+      '';
+      open_mapping = "[[<C-/>]]";
+      hide_numbers = true;
+      shade_terminals = true;
+      start_in_insert = true;
+      terminal_mappings = true;
+      persist_mode = true;
+      insert_mappings = true;
+      close_on_exit = true;
+      shell = "zsh";
+      direction = "horizontal"; # 'vertical' | 'horizontal' | 'tab' | 'float'
+      float_opts = {
+        border = "single"; # 'single' | 'double' | 'shadow' | 'curved' | ... other options supported by win open
+        width = 80;
+        height = 20;
+        winblend = 0;
+      };
     };
   };
 }
