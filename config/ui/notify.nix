5214a8d04ac370f850335877dd1af36bb1e8a641
spector700 <72362399+spector700@users.noreply.github.com>
2024-04-19 23:04:15 -0500

    feat(notify): remove animations for preformance

--- config/ui/notify.nix
+++ config/ui/notify.nix
@@ -1,10 +1,9 @@
 {
   plugins.notify = {
     enable = true;
-    backgroundColour = "#000000";
-    fps = 60;
-    render = "default";
-    timeout = 1000;
+    # remove animations for preformance
+    stages = "static";
+    timeout = 4000;
   };
   keymaps = [
     {
@@ -19,26 +18,26 @@
     }
   ];
   extraConfigLua = ''
-    			local notify = require("notify")
+    local notify = require("notify")
 
-    			local filtered_message = { "No information available" }
+    local filtered_message = { "No information available" }
 
-    			-- Override notify function to filter out messages
-    			---@diagnostic disable-next-line: duplicate-set-field
-    			vim.notify = function(message, level, opts)
-    				local merged_opts = vim.tbl_extend("force", {
-    					on_open = function(win)
-    						local buf = vim.api.nvim_win_get_buf(win)
-    						vim.api.nvim_buf_set_option(buf, "filetype", "markdown")
-    					end,
-    				}, opts or {})
+    -- Override notify function to filter out messages
+    ---@diagnostic disable-next-line: duplicate-set-field
+    vim.notify = function(message, level, opts)
+    	local merged_opts = vim.tbl_extend("force", {
+    		on_open = function(win)
+    			local buf = vim.api.nvim_win_get_buf(win)
+    			vim.api.nvim_buf_set_option(buf, "filetype", "markdown")
+    		end,
+    	}, opts or {})
 
-    				for _, msg in ipairs(filtered_message) do
-    					if message == msg then
-    						return
-    					end
-    				end
-    				return notify(message, level, merged_opts)
-    			end
+    	for _, msg in ipairs(filtered_message) do
+    		if message == msg then
+    			return
+    		end
+    	end
+    	return notify(message, level, merged_opts)
+    end
   '';
 }
