3466d2e44acddc7ed473234577cb82d80adbba99
spector700 <72362399+spector700@users.noreply.github.com>
2024-04-25 19:46:51 -0500

    feat(c++): add cmake lsp

--- config/lang/cpp.nix
+++ config/lang/cpp.nix
@@ -15,11 +15,14 @@
       };
     };
 
-    lsp.servers.clangd = {
-      enable = true;
-      onAttach.function = ''
-        vim.keymap.set('n', 'gh', "<cmd>ClangdSwitchSourceHeader<cr>", { desc = "Switch Source/Header (C/C++)", buffer = bufnr })
-      '';
+    lsp.servers = {
+      cmake.enable = true;
+      clangd = {
+        enable = true;
+        onAttach.function = ''
+          vim.keymap.set('n', 'gh', "<cmd>ClangdSwitchSourceHeader<cr>", { desc = "Switch Source/Header (C/C++)", buffer = bufnr })
+        '';
+      };
     };
   };
 }
