4876d9d722efd1baba4d0e8d38c7cf6a05b03e66
spector700 <72362399+spector700@users.noreply.github.com>
2024-08-22 20:07:46 -0500

    feat: added lspkind with our custom icons

--- config/completion/cmp.nix
+++ config/completion/cmp.nix
@@ -91,64 +91,6 @@
             require('luasnip').lsp_expand(args.body)
           end
         '';
-
-        formatting = {
-          fields = [
-            "abbr"
-            "kind"
-            "menu"
-          ];
-          format.__raw = ''
-            function(_, item)
-              local icons = {
-                Namespace = "󰌗",
-                Text = "󰊄",
-                Method = "󰆧",
-                Function = "󰡱",
-                Constructor = "",
-                Field = "󰜢",
-                Variable = "󰀫",
-                Class = "󰠱",
-                Interface = "",
-                Module = "󰕳",
-                Property = "",
-                Unit = "󰑭",
-                Value = "󰎠",
-                Enum = "",
-                Keyword = "󰌋",
-                Snippet = "",
-                Color = "󰏘",
-                File = "󰈚",
-                Reference = "󰈇",
-                Folder = "󰉋",
-                EnumMember = "",
-                Constant = "󰏿",
-                Struct = "󰙅",
-                Event = "",
-                Operator = "󰆕",
-                TypeParameter = "",
-                Table = "",
-                Object = "󰅩",
-                Tag = "",
-                Array = "[]",
-                Boolean = "",
-                Number = "",
-                Null = "󰟢",
-                String = "󰉿",
-                Calendar = "",
-                Watch = "󰥔",
-                Package = "",
-                Copilot = "",
-                Codeium = "",
-                TabNine = "",
-              }
-
-              local icon = icons[item.kind] or ""
-              item.kind = string.format("%s %s", icon, item.kind or "")
-              return item
-            end
-          '';
-        };
       };
     };
   };
--- config/lsp/lsp.nix
+++ config/lsp/lsp.nix
@@ -2,6 +2,67 @@
   plugins = {
     lsp.enable = true;
 
+    lspkind = {
+      enable = true;
+      cmp = {
+        menu = {
+          nvim_lsp = "[LSP]";
+          nvim_lua = "[api]";
+          path = "[path]";
+          luasnip = "[snip]";
+          buffer = "[buffer]";
+          neorg = "[neorg]";
+        };
+      };
+      symbolMap = {
+        Namespace = "󰌗";
+        Text = "󰊄";
+        Method = "󰆧";
+        Function = "󰡱";
+        Constructor = "";
+        Field = "󰜢";
+        Variable = "󰀫";
+        Class = "󰠱";
+        Interface = "";
+        Module = "󰕳";
+        Property = "";
+        Unit = "󰑭";
+        Value = "󰎠";
+        Enum = "";
+        Keyword = "󰌋";
+        Snippet = "";
+        Color = "󰏘";
+        File = "󰈚";
+        Reference = "󰈇";
+        Folder = "󰉋";
+        EnumMember = "";
+        Constant = "󰏿";
+        Struct = "󰙅";
+        Event = "";
+        Operator = "󰆕";
+        TypeParameter = "";
+        Table = "";
+        Object = "󰅩";
+        Tag = "";
+        Array = "[]";
+        Boolean = "";
+        Number = "";
+        Null = "󰟢";
+        String = "󰉿";
+        Calendar = "";
+        Watch = "󰥔";
+        Package = "";
+        Copilot = "";
+        Codeium = "";
+        TabNine = "";
+      };
+
+      extraOptions = {
+        maxwidth = 50;
+        ellipsis_char = "...";
+      };
+    };
+
     lint.enable = true;
   };
 
