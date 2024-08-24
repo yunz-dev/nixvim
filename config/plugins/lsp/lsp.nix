f809ac84473b9b7349fca6ba191a244dd169f495
spector700 <72362399+spector700@users.noreply.github.com>
2024-08-24 16:07:09 -0500

    feat: added icons to lsp

--- config/plugins/lsp/lsp.nix
+++ config/plugins/lsp/lsp.nix
@@ -1,43 +1,56 @@
+{ icons, ... }:
 {
   plugins = {
     lsp.enable = true;
-
     lint.enable = true;
   };
 
-  extraConfigLua = ''
-    local _border = "rounded"
+  extraConfigLua = # lua
+    ''
+      local _border = "rounded"
+      local signs = {
+          Hint = "${icons.diagnostics.BoldHint}",
+          Info = "${icons.diagnostics.BoldInformation}",
+          Warn = "${icons.diagnostics.BoldWarning}",
+          Error = "${icons.diagnostics.BoldError}",
+        }
 
-    vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(
-      vim.lsp.handlers.hover, {
-        border = _border,
+      vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(
+        vim.lsp.handlers.hover, {
+          border = _border,
+        })
+
+      vim.lsp.handlers["textDocument/signatureHelp"] = vim.lsp.with(
+        vim.lsp.handlers.signature_help, {
+          border = _border,
+        })
+
+
+
+      for type, icon in pairs(signs) do
+        local hl = "DiagnosticSign" .. type
+        vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = hl })
+      end
+
+      vim.diagnostic.config{
+        float = { border = _border },
+        virtual_text = true,
+        underline = true,
+        serverity_sort = true,
+        signs = true,
+        update_in_insert = true,
+      };
+
+      require('lspconfig.ui.windows').default_options = {
+        border = _border
+      }
+
+      vim.api.nvim_create_autocmd({ "BufWritePost" }, {
+        callback = function()
+          require("lint").try_lint()
+        end,
       })
-
-    vim.lsp.handlers["textDocument/signatureHelp"] = vim.lsp.with(
-      vim.lsp.handlers.signature_help, {
-        border = _border,
-      })
-
-    vim.diagnostic.config{
-      float = { border = _border },
-      virtual_text = {
-        prefix = "",
-      },
-      signs = true,
-      underline = true,
-      update_in_insert = true,
-    };
-
-    require('lspconfig.ui.windows').default_options = {
-      border = _border
-    }
-
-    vim.api.nvim_create_autocmd({ "BufWritePost" }, {
-      callback = function()
-        require("lint").try_lint()
-      end,
-    })
-  '';
+    '';
 
   keymaps = [
     {
