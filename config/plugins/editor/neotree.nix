d21362b20641845d79ae4778fad256fb84ba4ee8
spector700 <72362399+spector700@users.noreply.github.com>
2024-08-24 14:10:26 -0500

    feat:(neotree) added some settings

--- config/plugins/editor/neotree.nix
+++ config/plugins/editor/neotree.nix
@@ -2,12 +2,34 @@
   plugins.neo-tree = {
     enable = true;
     closeIfLastWindow = true;
+    sources = [
+      "filesystem"
+      "buffers"
+      "git_status"
+      "document_symbols"
+    ];
     popupBorderStyle = "rounded"; # “NC”, “double”, “none”, “rounded”, “shadow”, “single”, “solid” or raw lua code
 
     filesystem = {
       bindToCwd = false;
       useLibuvFileWatcher = true;
       followCurrentFile.enabled = true;
+    };
+
+    defaultComponentConfigs = {
+      gitStatus = {
+        symbols = {
+          added = " ";
+          conflict = "󰩌 ";
+          deleted = "󱂥";
+          ignored = " ";
+          modified = " ";
+          renamed = "󰑕";
+          staged = "󰩍";
+          unstaged = "";
+          untracked = "";
+        };
+      };
     };
 
     window.mappings = {
@@ -19,7 +41,7 @@
     {
       mode = "n";
       key = "<leader>e";
-      action = "<cmd>Neotree toggle reveal_force_cwd<cr>";
+      action = "<cmd>Neotree toggle<cr>";
       options = {
         silent = true;
         desc = "Explorer NeoTree (root dir)";
