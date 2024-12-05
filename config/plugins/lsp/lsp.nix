08b805833fdd0179d107a09e06193d09804dd4e8
spector700 <72362399+spector700@users.noreply.github.com>
2024-12-05 13:49:56 -0600

    feat: add lsp-signature

--- config/plugins/lsp/lsp.nix
+++ config/plugins/lsp/lsp.nix
@@ -1,6 +1,7 @@
-{ icons, ... }:
 {
   plugins = {
+    lsp-signature.enable = true;
+
     lsp = {
       enable = true;
       servers.typos_lsp.enable = true;
@@ -11,53 +12,6 @@
     };
     lint.enable = true;
   };
-
-  # extraConfigLua = # lua
-  #   ''
-  #     local _border = "rounded"
-  #     local signs = {
-  #         Hint = "${icons.diagnostics.BoldHint}",
-  #         Info = "${icons.diagnostics.BoldInformation}",
-  #         Warn = "${icons.diagnostics.BoldWarning}",
-  #         Error = "${icons.diagnostics.BoldError}",
-  #       }
-  #
-  #     vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(
-  #       vim.lsp.handlers.hover, {
-  #         border = _border,
-  #       })
-  #
-  #     vim.lsp.handlers["textDocument/signatureHelp"] = vim.lsp.with(
-  #       vim.lsp.handlers.signature_help, {
-  #         border = _border,
-  #       })
-  #
-  #
-  #
-  #     for type, icon in pairs(signs) do
-  #       local hl = "DiagnosticSign" .. type
-  #       vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = hl })
-  #     end
-  #
-  #     vim.diagnostic.config{
-  #       float = { border = _border },
-  #       virtual_text = true,
-  #       underline = true,
-  #       serverity_sort = true,
-  #       signs = true,
-  #       update_in_insert = true,
-  #     };
-  #
-  #     require('lspconfig.ui.windows').default_options = {
-  #       border = _border
-  #     }
-  #
-  #     vim.api.nvim_create_autocmd({ "BufWritePost" }, {
-  #       callback = function()
-  #         require("lint").try_lint()
-  #       end,
-  #     })
-  #   '';
 
   keymaps = [
     {
