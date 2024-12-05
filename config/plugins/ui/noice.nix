db474884510952d8712b6df49fcd8d25d33b018f
spector700 <72362399+spector700@users.noreply.github.com>
2024-12-05 13:41:33 -0600

    feat(noice): add settings

--- config/plugins/ui/noice.nix
+++ config/plugins/ui/noice.nix
@@ -8,16 +8,18 @@
           "vim.lsp.util.stylize_markdown" = true;
           "cmp.entry.get_documentation" = true;
         };
+        progress.enabled = true;
       };
 
       presets = {
         bottom_search = true;
         command_palette = true;
         long_message_to_split = true;
-        inc_rename = false;
-        lsp_doc_border = false;
+        inc_rename = true;
+        lsp_doc_border = true;
       };
       notify.enabled = true;
+
       routes = [
         {
           filter = {
