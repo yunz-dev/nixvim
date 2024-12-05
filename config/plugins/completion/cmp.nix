a50e63e67526ac37f05bc2ca0ce779674bacc87f
spector700 <72362399+spector700@users.noreply.github.com>
2024-12-04 20:40:50 -0600

    feat: add cmp sources

--- config/plugins/completion/cmp.nix
+++ config/plugins/completion/cmp.nix
@@ -1,13 +1,15 @@
 {
   plugins = {
-    cmp-nvim-lsp.enable = true;
-    cmp-emoji.enable = true;
-    cmp-buffer.enable = true;
-    cmp-path.enable = true;
-    cmp_luasnip.enable = true;
-    cmp-cmdline.enable = true;
+    # cmp-nvim-lsp.enable = true;
+    # cmp-emoji.enable = true;
+    # cmp-buffer.enable = true;
+    # cmp-path.enable = true;
+    # cmp_luasnip.enable = true;
+    # cmp-cmdline.enable = true;
     cmp = {
       enable = true;
+      autoEnableSources = true;
+
       cmdline = {
         "/" = {
           mapping.__raw = "cmp.mapping.preset.cmdline()";
@@ -39,16 +41,62 @@
         # Preselect first entry
         completion.completeopt = "menu,menuone,noinsert";
         sources = [
-          { name = "nvim_lsp"; } # lsp
-          { name = "luasnip"; }
-          # { name = "copilot"; }
+          {
+            name = "nvim_lsp";
+            priority = 100;
+          }
+          {
+            name = "nvim_lsp_signature_help";
+            priority = 100;
+          }
+          {
+            name = "nvim_lsp_document_symbol";
+            priority = 100;
+          }
+          {
+            name = "treesitter";
+            priority = 80;
+          }
+          {
+            name = "luasnip";
+            priority = 70;
+          }
+          {
+            name = "codeium";
+            priority = 60;
+          }
           {
             name = "buffer";
+            priority = 50;
             # Words from other open buffers can also be suggested.
             option.get_bufnrs.__raw = "vim.api.nvim_list_bufs";
             keywordLength = 3;
           }
-          { name = "path"; }
+          {
+            name = "path";
+            priority = 30;
+          }
+          {
+            name = "git";
+            priority = 20;
+          }
+          {
+            name = "npm";
+            priority = 20;
+          }
+          {
+            name = "zsh";
+            priority = 20;
+          }
+          {
+            name = "calc";
+            priority = 10;
+          }
+          {
+            name = "emoji";
+            priority = 5;
+          }
+          { name = "nixpkgs_maintainers"; }
         ];
 
         window = {
@@ -80,22 +128,32 @@
             end, { "i", "s" })
           '';
 
-          "<c-n>" = "cmp.mapping(cmp.mapping.select_next_item())";
-          "<c-p>" = "cmp.mapping(cmp.mapping.select_prev_item())";
-          "<c-e>" = "cmp.mapping.abort()";
-          "<C-d>" = "cmp.mapping.scroll_docs(-4)";
-          "<C-f>" = "cmp.mapping.scroll_docs(4)";
-          "<Up>" = "cmp.mapping(cmp.mapping.select_prev_item(), {'i', 's'})";
-          "<Down>" = "cmp.mapping(cmp.mapping.select_next_item(), {'i', 's'})";
-          "<CR>" = "cmp.mapping.confirm({ behavior = cmp.ConfirmBehavior.Insert, select = true })";
-          "<C-Space>" = "cmp.mapping.complete()";
+          "<C-n>" = # lua
+            "cmp.mapping(cmp.mapping.select_next_item())";
+          "<C-p>" = # lua
+            "cmp.mapping(cmp.mapping.select_prev_item())";
+          "<C-e>" = # lua
+            "cmp.mapping.abort()";
+          "<C-d>" = # lua
+            "cmp.mapping.scroll_docs(-4)";
+          "<C-f>" = # lua
+            "cmp.mapping.scroll_docs(4)";
+          "<Up>" = # lua
+            "cmp.mapping(cmp.mapping.select_prev_item(), {'i', 's'})";
+          "<Down>" = # lua
+            "cmp.mapping(cmp.mapping.select_next_item(), {'i', 's'})";
+          "<CR>" = # lua
+            "cmp.mapping.confirm({ behavior = cmp.ConfirmBehavior.Insert, select = true })";
+          "<C-Space>" = # lua
+            "cmp.mapping.complete()";
         };
 
-        snippet.expand = ''
-          function(args)
-            require('luasnip').lsp_expand(args.body)
-          end
-        '';
+        snippet.expand = # lua
+          ''
+            function(args)
+              require('luasnip').lsp_expand(args.body)
+            end
+          '';
       };
     };
   };
