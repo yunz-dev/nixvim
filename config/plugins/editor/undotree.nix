13f01a4e0157a7a55585b7785978e71cdb3b9067
spector700 <72362399+spector700@users.noreply.github.com>
2024-12-04 18:21:55 -0600

    feat(undotree): add settings

--- config/plugins/editor/undotree.nix
+++ config/plugins/editor/undotree.nix
@@ -4,6 +4,19 @@
     settings = {
       autoOpenDiff = true;
       focusOnToggle = true;
+      CursorLine = true;
+      DiffAutoOpen = true;
+      DiffCommand = "diff";
+      DiffpanelHeight = 10;
+      HelpLine = true;
+      HighlightChangedText = true;
+      HighlightChangedWithSign = true;
+      HighlightSyntaxAdd = "DiffAdd";
+      HighlightSyntaxChange = "DiffChange";
+      HighlightSyntaxDel = "DiffDelete";
+      RelativeTimestamp = true;
+      SetFocusWhenToggle = true;
+      ShortIndicators = false;
     };
   };
 
